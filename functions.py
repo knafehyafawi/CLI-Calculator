ecfd972d3ef0afa862511dfa567155483d19e45c
Knafeh <adnanawad315@gmail.com>
2025-05-14 12:07:04 -0400

    successfully implemented 2 argument calculator that takes 4 arithmetic operations: / * - +.

--- functions.py
+++ functions.py
@@ -1,8 +1,15 @@
-def addition(a, b):
-    return a + b
-def subtraction(a, b):
-    return a - b
-def multiplication(a, b):
-    return a * b
-def division(a, b):
-    return a / b
+def calculate(num1, num2, operation):
+    if operation == '+':
+        ans = num1 + num2
+    elif operation == '-':
+        ans = num1 - num2
+    elif operation == '*':
+        ans = num1 * num2
+    elif operation == '/':
+        if num2 == 0:
+            ans = "Syntax error. Cannot divide by zero."
+        else:
+            ans = num1 / num2
+    else:
+        ans = "Syntax error. Operation not recognized."
+    return ans
--- main.py
+++ main.py
@@ -1,22 +1,11 @@
-from pandas.core.ops import arithmetic_op
-
 from functions import *
 
-def main():
-
-    value1 = input("Enter first number: ")
-
-    # select mode: addition, subtraction, etc
-    arithmetic_method = input()
-    if arithmetic_method == "+":
-        print("+")
-    if arithmetic_method == "-":
-        print("-")
-    if arithmetic_method == "*":
-        print("*")
-    if arithmetic_method == "/":
-        print("/")
-
-    value2 = input("Enter second number: ")
-
+def __main__():
+    num1 = float(input("Enter first number: "))
+    num2 = float(input("Enter second number: "))
+    operation = input("Enter operation: ")
+    ans = calculate(num1, num2, operation)
+    print(ans)
     return
+
+__main__()
